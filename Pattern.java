6e3eaeaacab60e7db5cea9cb489eb9ae29b07dc2
Rajarshi07 <rajarshighoshdastidar@gmail.com>
2023-01-22 00:43:44 +0530

    Added .gitignore

--- Pattern.java
+++ Pattern.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
+
 public class Pattern {
     public static void main(String[] args) {
-        int n=3;
+        Scanner in=new Scanner(System.in);
+        System.out.print("Enter the number of rows: ");
+        int n=in.nextInt();
+        in.close();
         for(int i=0;i<n;i++) {
             for(int k=0;k<i;k++)
                 System.out.print(" ");
